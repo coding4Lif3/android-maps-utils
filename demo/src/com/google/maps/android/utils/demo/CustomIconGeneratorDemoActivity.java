80769a1122a85c8f083af660d57736a696df1218
federico <f.monaco@ips-intelligence.com>
2014-05-05 11:26:26 +0200

    Added some drawables for demo purpose

--- demo/src/com/google/maps/android/utils/demo/CustomIconGeneratorDemoActivity.java
+++ demo/src/com/google/maps/android/utils/demo/CustomIconGeneratorDemoActivity.java
@@ -19,19 +19,19 @@ public class CustomIconGeneratorDemoActivity extends BaseDemoActivity {
         addIcon(iconFactory, "Blue style\nasdsadsadasdsad\nsakdjsakldjasl", new LatLng(41.59223, 12.5036633));
 
 
-        // default with same icon
         addIcon(iconFactory, "Default", new LatLng(-33.8696, 151.2094));
 
         iconFactory.setStyle(IconGenerator.STYLE_BLUE);
         addIcon(iconFactory, "Blue style", new LatLng(-33.9360, 151.2070));
 
         iconFactory.setRotation(90);
-        iconFactory.setCustomStyle(R.drawable.car_icon_green, com.google.maps.android.R.style.Bubble_TextAppearance_Light
+        iconFactory.setCustomStyle(R.drawable.car_icon_red, com.google.maps.android.R.style.Bubble_TextAppearance_Light
         );
         addIcon(iconFactory, "Rotated 90 degrees", new LatLng(-33.8858, 151.096));
 
         iconFactory.setContentRotation(-90);
-        iconFactory.setStyle(IconGenerator.STYLE_PURPLE);
+        iconFactory.setCustomStyle(R.drawable.car_icon_white, com.google.maps.android.R.style.Bubble_TextAppearance_Dark
+        );
         addIcon(iconFactory, "Rotate=90, ContentRotate=-90", new LatLng(-33.9992, 151.098));
 
         iconFactory.setRotation(0);
